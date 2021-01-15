4fa0aa4ae9c26bb85252d3fac0bfb99b4b59c4fe
JoselynCastro <joselyncastro.edif@gmail.com>
2021-01-15 14:05:35 -0300

    ejercicios basicos

--- Java/ActividadesCoding/src/Actividades/BasicJava.java
+++ Java/ActividadesCoding/src/Actividades/BasicJava.java
@@ -1,13 +1,14 @@
 package Actividades;
+import java.util.ArrayList;
 
 public final class BasicJava{
-	public static void imprime(int num){
+	public void imprime(int num){
 		int i=0;
 		for(i=1; i<=num; i++) {
 		   System.out.println(i);
 	 }
    }
-   public static void impares(int n) {
+   public void impares(int n) {
 	   int i=0;
 	   for(i=1; i<=n; i++) {
 	     if(i%2!=0) {
@@ -15,7 +16,7 @@ public final class BasicJava{
 	     }
 	   }
    }
-   public static void suma(int sum) {
+   public void suma(int sum) {
 	   int i=0;
 	   int suma=0;
 	   for(i=0; i<=sum; i++) {
@@ -23,10 +24,107 @@ public final class BasicJava{
 		   System.out.println("Nuevo número:"+i+" Suma:"+suma+"");
 	   }
    }
-   public static void recorrer(ArrayList <Integer> list) {
-	   for(int i = 0; i<=list.length; i++) {
-		   System.out.println(list[i]);
+   public void recorrer(int [] x) {
+	   for(int i = 0; i<x.length; i++) {
+		   System.out.println(x[i]);
 	   }
 	   
    }
+   public int maximo(int [] a) {
+	   int maximo=a[0];
+	   for(int i = 0; i<a.length; i++) {
+		   maximo=Math.max(maximo, a[i]);
+	   }
+	   return maximo;
+   }
+
+   public int promedio(int [] z) {
+	   int prom=0;
+	   for(int i = 0; i<z.length; i++) {
+		   prom=z[i]+prom;
+	   }
+	   return(prom/z.length);
+   }
+
+   
+   public ArrayList<Integer> arrImp(int b) {
+	   ArrayList<Integer> myArray = new ArrayList<Integer>();
+	   for(int i = 0; i<b; i++) {
+		   if(i%2!=0) {
+			  myArray.add(i); 
+		   }
+	   }
+	   return myArray;
+   }
+   
+//   public ArrayList<Integer> mayores(int []f, int m) {
+//	   ArrayList<Integer> array = new ArrayList<Integer>();
+//	   var temp=0;
+//	   for(int i = 0; i<f.length; i++) {
+//		   if(f[i]>m) {
+//		   array.add(f[i]);
+//	     }
+//	   }
+//	   return (array.length);//editar
+//   }
+  
+   public ArrayList<Integer> potencia(int []s) {
+	   ArrayList<Integer> array = new ArrayList<Integer>();
+	   for(int i = 0; i<s.length; i++) {
+		   int temp = i;
+		   i= (int) Math.pow(s[i],2);
+		   array.add(i);
+		   i=temp;
+	   }
+	   return array;
+   }
+   
+   public ArrayList<Integer> chao(int []s) {
+	   ArrayList<Integer> array = new ArrayList<Integer>();
+	   for(int i = 0; i<s.length; i++) {
+		   if(s[i]<0) {
+		       array.add(0);
+	       }
+		   else {
+			   array.add(s[i]);
+		   }
+	   }
+	   return array;
+   }
+   
+   public ArrayList<Integer> mmp(int []s) {
+	   ArrayList<Integer> array = new ArrayList<Integer>();
+	   int max=0;
+	   int min= s[0];
+	   int suma=0;
+	   for(int i = 0; i<s.length; i++) {
+		   suma=suma+s[i];
+		   if(s[i]>max) {
+		       max=s[i];
+	       }
+		   if(s[i]<min){
+			   min=s[i];
+		   }
+	   }
+	   array.add(max);
+	   array.add(min);
+	   array.add(suma/s.length);
+	   return array;
+   }
+   
+   public ArrayList<Integer> cambio(int []r) {
+	   ArrayList<Integer> array = new ArrayList<Integer>();
+	   var temp=0;
+	   for(int i = 0; i<r.length; i++) {
+		   if(i==0) {
+		       r[i]=temp;
+	       }
+		   else {
+			   array.add(r[i]);
+		   }
+	   }
+	   array.add(temp);
+	   return array;
+   }
+   
 }
--- Java/ActividadesCoding/src/Actividades/TestBasicJava.java
+++ Java/ActividadesCoding/src/Actividades/TestBasicJava.java
@@ -1,13 +1,38 @@
 package Actividades;
 
 public class TestBasicJava {
+
 	public static void main(String[] args) {
 		BasicJava ejercicios= new BasicJava();
-		int num = 255;
-		int i= BasicJava.imprime(0);
+		ejercicios.imprime(255);
 		
-		BasicJava.impares(0);
-		BasicJava.suma(0);
-		BasicJava.recorrer();
+		ejercicios.impares(255);
+		
+		ejercicios.suma(255);
+		
+		int[] x= {1,3,5,7,9,13};
+		ejercicios.recorrer(x);
+		
+		int[] a= {-3,5,7};
+		System.out.println(ejercicios.maximo(a));
+		
+		int [] z= {2,10,3};
+		System.out.println(ejercicios.promedio(z));
+		
+		System.out.println(ejercicios.arrImp(255));
+		
+//		int[ ]f= {1,3,5,7};
+//		int m=3;
+//		System.out.println(ejercicios.mayores(f,m));
+		
+		int[ ]s= {1,5,10,-2};
+    	System.out.println(ejercicios.potencia(s));
+    	
+    	System.out.println(ejercicios.chao(s));
+    	
+    	System.out.println(ejercicios.mmp(s));
+    	
+    	int[]r= {1,5,10,7,-2};
+    	System.out.println(ejercicios.cambio(r));
 	}
 }
