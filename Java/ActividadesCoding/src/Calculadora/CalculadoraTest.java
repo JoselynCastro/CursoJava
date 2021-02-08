2c543c55a245daa112e5198a72d56c9909367cb3
JoselynCastro <joselyncastro.edif@gmail.com>
2021-02-08 10:19:38 -0300

    calculadora1

---
+++ Java/ActividadesCoding/src/Calculadora/CalculadoraTest.java
@@ -0,0 +1,13 @@
+package Calculadora;
+
+public class CalculadoraTest {
+	public static void main(String[] args) {
+		Calculadora act1=new Calculadora(10.5, "+", 5.2);
+		act1.performOperation();
+		System.out.println(act1.getResultado());
+		
+		Calculadora act2=new Calculadora(27, "-", 26);
+		act2.performOperation();
+		System.out.println(act2.getResultado());
+	}
+}
